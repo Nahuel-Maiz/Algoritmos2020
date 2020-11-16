9d7da0d6290189f28b29e6ab54ff436aac29fc58
Nahuel-Maiz <63472764+Nahuel-Maiz@users.noreply.github.com>
2020-11-16 18:02:30 -0300

    Create TDA_pila.py

---
+++ TDA_pila.py
@@ -0,0 +1,33 @@
+class Pila():
+    """Crear una pila vacia"""
+    def __init__(self):
+        self.cima = -1
+        self.datos = [0] * 5
+
+
+def apilar(pila, dato):
+    """apile el dato en la cima"""
+    pila.cima += 1
+    pila.datos[pila.cima] = dato
+
+
+def desapilar(pila):
+    dato = pila.datos[pila.cima]
+    pila.cima -= 1
+    return dato
+
+
+def pila_vacia(pila):
+    return pila.cima == -1
+
+
+def pila_llena(pila):
+    return pila.cima == len(pila.datos)-1
+
+
+def tamanio(pila):
+    return pila.cima + 1
+
+
+def cima(pila):
+    return pila.datos[pila.cima]
